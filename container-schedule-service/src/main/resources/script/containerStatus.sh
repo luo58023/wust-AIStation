c5a97f99832d1b04f8570d1fad997a46ea36cabe
luo58023 <41656429+luo58023@users.noreply.github.com>
2019-02-22 21:12:54 +0800

    Delete containerStatus.sh

--- container-schedule-service/src/main/resources/script/containerStatus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-podName=$1
-result=$(kubectl get pod -n kube-system|grep ${podName} |awk '{print $3}')
-if [ "$result" == "Running" ]; then
-   echo true
-else
-   echo "not running"
-   exit 1;
-fi
